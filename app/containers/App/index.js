a9c7383a0e115652ccc46123af80724c9c3f050d
cuongnq072610 <cuongnqse05240@fpt.edu.vn>
2020-01-12 12:09:52 +0700

    config webpack + create menu

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -8,41 +8,38 @@
 
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import styled from 'styled-components';
 import { Switch, Route } from 'react-router-dom';
+import 'antd/dist/antd.css';
 
 import HomePage from 'containers/HomePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
 
-import GlobalStyle from '../../global-styles';
+import { Layout } from 'antd';
 
-const AppWrapper = styled.div`
-  max-width: calc(768px + 16px * 2);
-  margin: 0 auto;
-  display: flex;
-  min-height: 100%;
-  padding: 0 16px;
-  flex-direction: column;
-`;
+import GlobalStyle from '../../global-styles';
+import SideMenu from '../../components/Menu';
 
 export default function App() {
   return (
-    <AppWrapper>
+    <Layout>
       <Helmet
         titleTemplate="Smart Course Admin"
         defaultTitle="Smart Course Admin"
       >
         <meta name="description" content="Smart Course Management Admin" />
       </Helmet>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="" component={NotFoundPage} />
-      </Switch>
-      <Footer />
+      <SideMenu />
+      <Layout>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="" component={NotFoundPage} />
+        </Switch>
+        <Footer />
+      </Layout>
       <GlobalStyle />
-    </AppWrapper>
+    </Layout>
   );
 }
